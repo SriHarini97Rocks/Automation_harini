f9a309d7ecd1866ab1343d051eaa53514e526243
SriHarini97Rocks <sjhari26@gmail.com>
2022-05-11 19:07:46 +0530

    sample commit 2

---
+++ TestProject1/HomePage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    class HomePage
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.Id, Using = "account")]
+        public IWebElement MyAccount { get; set; }
+    }
+}
